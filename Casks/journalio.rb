771950e5c8d48d6a4df47c198c98a070509f103d
Forest0923 <masahirom0923@gmail.com>
2024-12-01 14:05:21 +0900

    Add journalio.rb

---
+++ Casks/journalio.rb
@@ -0,0 +1,11 @@
+cask "journalio" do
+  version "1.0.3"
+  sha256 "7a5204aeed2ae1b320e83ed4ddd6931b14cff1fd16ea057dbb91b443af33ed16"
+
+  url "https://github.com/Forest0923/journalio/releases/download/v#{version}/JournalIO_#{version}_aarch64.dmg"
+  name "journalio"
+  desc ""
+  homepage ""
+
+  app "JournalIO.app"
+end
